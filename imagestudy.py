943a76308a6830b0efdd99199605ca6a907cb7f2
Angad Bajwa <49759550+therealAngad@users.noreply.github.com>
2020-03-26 21:59:32 +0530

    Update imagestudy.py

--- imagestudy.py
+++ imagestudy.py
@@ -7,4 +7,4 @@ print(img1.dtype)   # data type of numbers in array
 print(img1.size)    #size= 512*512*3
 print(img1.ndim)    #number of dimensions=3 .....Blue Green Red
 print(img1)         #image in form of an n dimension array
-
+print(img1.shape)
